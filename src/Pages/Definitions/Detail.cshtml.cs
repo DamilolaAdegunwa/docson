d48c703ead482df5aec6be7b0e27d7b09692e763
Thomas Duft <thomas.duft@outlook.com>
2019-02-13 16:17:29 +0100

    added DefinitionsDetail page

---
+++ src/Pages/Definitions/Detail.cshtml.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using tomware.Docson.Services;
+
+namespace tomware.Docson.Pages
+{
+  public class DefinitionsDetailModel : PageModel
+  {
+    private readonly IMessageDefinitionService _service;
+
+    public MessageDefinition Defintion { get; set; }
+
+    public DefinitionsDetailModel(
+      IMessageDefinitionService service
+    )
+    {
+      _service = service;
+    }
+
+    public void OnGet(string id)
+    {
+      this.Defintion = _service.GetByName(id);
+    }
+  }
+}
